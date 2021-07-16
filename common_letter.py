8a7654bce1fd154b9056dd1e14ecae9fe6b7923f
fander2468 <86483561+fander2468@users.noreply.github.com>
2021-07-16 15:35:14 -0400

    first commit

---
+++ common_letter.py
@@ -0,0 +1,13 @@
+#  Make a function that takes a string and returns the most common letter in the string regardless of capitalization.
+#  ignore spaces
+#  ex) "John Loves to Play Baseball and Joanie Loves to Play Basketball, but Jenny Likes To Dance"
+#  output: l
+
+
+s = "John Loves to Play Baseball and Joanie Loves to Play Basketball, but Jenny Likes To Dance"
+
+def common_letter(some_string):
+    
+
+
+common_letter(s.lower())
